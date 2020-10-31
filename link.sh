a2e465797e064985094e5f7195730e3e1ef24e56
Jamie Hitchen <jamie.hitchen@oddschecker.com>
2020-10-31 15:30:57 +0000

    add link.sh

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash 
+
+ln -s $(pwd)/commitbranch /usr/local/bin/cb
+ln -s $(pwd)/compare /usr/local/bin/compare
+ln -s $(pwd)/tolower /usr/local/bin/tolower
+ln -s $(pwd)/toupper /usr/local/bin/toupper
+ln -s $(pwd)/inside-outside/in /usr/local/bin/in
+ln -s $(pwd)/inside-outside/out /usr/local/bin/out
